321343f8a28e36e1f31f2a899597bed6ae28c837
lvccomputer <41769622+lvccomputer@users.noreply.github.com>
2018-12-14 00:11:07 +0700

    up project
    tải project lên lần đầu, hoàn thành footer

---
+++ js/trangchu.js
@@ -0,0 +1,43 @@
+function move_left(vt){
+	var a = vt.replace("px","");
+	a = Number(a);
+	if(a == 0){
+		a = -5360;
+	}
+	a = a+670;
+	document.getElementById('ul').style.left = a + "px";
+	document.getElementById('ul').style.transition = "right 1s";
+}
+function move_right(vt){
+	var a = vt.replace("px","");
+	a = Number(a);
+	if(a == -4690){
+		a = 670;
+	}
+	a = a-670;
+	document.getElementById('ul').style.left = a + "px";
+	document.getElementById('ul').style.transition = "left 1s";
+}
+function move_left2(vt){
+	var a = vt.replace("px","");
+	a = Number(a);
+	if(a == 0){
+		a = -2010;
+	}
+	a = a+670;
+	document.getElementById('ul2').style.left = a + "px";
+	document.getElementById('ul2').style.transition = "left 1s";
+}
+function move_right2(vt){
+	var a = vt.replace("px","");
+	a = Number(a);
+	if(a == -1340){
+		a = 670;
+	}
+	a = a-670;
+	document.getElementById('ul2').style.left = a + "px";
+	document.getElementById('ul2').style.transition = "left 1s";
+}
+function maudo(a){
+	a.style.backgroudColor = '#000';
+}
